74ee7f1c2a0dcc9c53429fdb19fd40a9b361cd75
Timothy B. Smith <smith@ttimsmith.com>
2019-01-25 14:31:02 -0800

    Add section list and animation

--- assets/app.js
+++ assets/app.js
@@ -4,6 +4,7 @@
 {% capture scripts %}
   {% include javascripts/heading-anchors.js %}
   {% include javascripts/group-list.js %}
+  {% include javascripts/topics-menu.js %}
   {% include javascripts/toc.js %}
 {% endcapture %}
 {{ scripts | strip }}
