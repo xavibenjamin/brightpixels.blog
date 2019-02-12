c69df34764a59ac72b523a7aa08b6d9949e27e8f
Tim Smith <smith@ttimsmith.com>
2019-02-11 21:08:13 -0600

    Add Dark Theme (#10)
    * Add new theme to default layout
    
    * Fix bc ad display
    
    * Switch to whitney for the series descriptor
    
    * add dark theme
    
    * Dark mode toggle styles
    
    * Add JS for dark mode toggle to work
    
    * Add dark mode toggle markup
    
    * Fix opacity on focus

--- assets/app.js
+++ assets/app.js
@@ -6,5 +6,6 @@
   {% include javascripts/group-list.js %}
   {% include javascripts/topics-menu.js %}
   {% include javascripts/toc.js %}
+  {% include javascripts/dark-mode.js %}
 {% endcapture %}
 {{ scripts | strip }}
